504396cf5593940f8a26610667bb38d93302bb08
IntegrityT18 <pappichu18@gmail.com>
2023-09-13 09:11:33 -0500

    Update 2

--- app/src/main/java/com/example/actualappssean/UpdateInfo.java
+++ app/src/main/java/com/example/actualappssean/UpdateInfo.java
@@ -1,5 +1,6 @@
 package com.example.actualappssean;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,13 +12,17 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class UpdateInfo extends AppCompatActivity {
-    private EditText editTextUpdateEmail, editTextUpdatePwd;
-    private ProgressBar progressBar;
-    private FirebaseAuth auth;
+    public EditText editTextUpdateEmail, editTextUpdatePwd, editTextOldEmail, editTextOldPwd;
+    public ProgressBar progressBar;
+    public FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +32,36 @@ public class UpdateInfo extends AppCompatActivity {
         buttonUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String textEmail = editTextUpdateEmail.getText().toString();
-                String textPwd = editTextUpdatePwd.getText().toString();
+                String oldTextEmail = editTextOldEmail.getText().toString();
+                String oldTextPwd = editTextOldPwd.getText().toString();
+                String newTextEmail = editTextUpdateEmail.getText().toString();
+                String newTextPwd = editTextUpdatePwd.getText().toString();
                 // Check to see if data is valid before registering
-                if (TextUtils.isEmpty(textEmail)) {
-                    Toast.makeText(UpdateInfo.this, "Please enter a new email address", Toast.LENGTH_SHORT).show();
-                    editTextUpdateEmail.setError("An email address is required!");
-                    editTextUpdateEmail.requestFocus();
-                } else if (TextUtils.isEmpty(textPwd) || textPwd.length() < 6) {
-                    Toast.makeText(UpdateInfo.this, "Please enter a new and valid password (6+ characters)", Toast.LENGTH_SHORT).show();
-                    editTextUpdatePwd.setError("A valid password is required!");
-                    editTextUpdatePwd.requestFocus();
-                } else {
-                    progressBar.setVisibility(View.VISIBLE);
-                    Toast.makeText(UpdateInfo.this, "Succesfully Updated!", Toast.LENGTH_SHORT).show();
-                    updateInfo(textEmail, textPwd);
-                }
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                AuthCredential credential = EmailAuthProvider.getCredential(oldTextEmail, oldTextPwd);
+                user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()) {
+                            if (TextUtils.isEmpty(newTextEmail)) {
+                                Toast.makeText(UpdateInfo.this, "Please enter a new email address", Toast.LENGTH_SHORT).show();
+                                editTextUpdateEmail.setError("An email address is required!");
+                                editTextUpdateEmail.requestFocus();
+                            } else if (TextUtils.isEmpty(newTextPwd) || newTextPwd.length() < 6) {
+                                Toast.makeText(UpdateInfo.this, "Please enter a new and valid password (6+ characters)", Toast.LENGTH_SHORT).show();
+                                editTextUpdatePwd.setError("A valid password is required!");
+                                editTextUpdatePwd.requestFocus();
+                            } else {
+                                progressBar.setVisibility(View.VISIBLE);
+                                updateInfo(newTextEmail, newTextPwd);
+                                Toast.makeText(UpdateInfo.this, "Succesfully Updated!", Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(UpdateInfo.this, "User does not exist, please sign up", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
             }
         });
 
@@ -59,7 +78,8 @@ public class UpdateInfo extends AppCompatActivity {
     }
 
     private void updateInfo(String updateEmail, String updatePwd) {
-        auth.getCurrentUser().updateEmail(updateEmail);
-        auth.getCurrentUser().updatePassword(updatePwd);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user.updateEmail(updateEmail);
+        user.updatePassword(updatePwd);
     }
 }
